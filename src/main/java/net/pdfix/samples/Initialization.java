2081169146604d7625cc6115cbfb26cf3ef95f88
jbaranec <jozef.baranec@gmail.com>
2023-03-28 21:36:32 +0200

    binary paths updated

--- src/main/java/net/pdfix/samples/Initialization.java
+++ src/main/java/net/pdfix/samples/Initialization.java
@@ -10,21 +10,22 @@ import net.pdfix.pdfixlib.Pdfix;
 import static java.util.Objects.isNull;
 
 public class Initialization {
-    
+
     private static String getLibraryName(String name) throws Exception {
 
         String os = System.getProperty("os.name").toLowerCase();
         if ((os.indexOf("mac") >= 0) || (os.indexOf("darwin") >= 0)) {
-            return "/darwin/lib" + name + ".dylib";
-        }
-        else if (os.indexOf("win") >= 0) {
+            if ("aarch64".equals(System.getProperty("os.arch")))
+                return "/arm64/lib" + name + ".dylib";
+            else
+                return "/x64/lib" + name + ".dylib";
+        } else if (os.indexOf("win") >= 0) {
             if ("64".equals(System.getProperty("sun.arch.data.model")))
                 return "/x64/" + name + ".dll";
             else
                 return "/x86/" + name + ".dll";
-        }
-        else if (os.indexOf("nux") >= 0) {
-            return "/linux/lib" + name + ".so";
+        } else if (os.indexOf("nux") >= 0) {
+            return "/x64/lib" + name + ".so";
         }
         throw new Exception("Unsupported platform " + os);
     }
@@ -32,35 +33,34 @@ public class Initialization {
     public static void run(String libPath) throws Exception {
 
         // load pdfix libraries
-        System.load(libPath + "/" + getLibraryName("pdfix"));
+        System.load(libPath + "/" + getLibraryName("pdf"));
         System.load(libPath + "/" + getLibraryName("ocr_tesseract"));
 
-        Pdfix pdfix = new Pdfix();        
+        Pdfix pdfix = new Pdfix();
         if (isNull(pdfix))
             throw new Exception("Pdfix initialization fail");
 
-        System.out.println( "PDFix SDK " + pdfix.GetVersionMajor() + "." +
+        System.out.println("PDFix SDK " + pdfix.GetVersionMajor() + "." +
                 pdfix.GetVersionMinor() + "." + pdfix.GetVersionPatch());
-            
+
         // license authorization
         if (!pdfix.GetAuthorization().IsAuthorized()) {
             String licenseName = "";
             String licenseKey = "";
-            if (licenseName.isEmpty()) {
+            if (licenseName.isEmpty() && !licenseKey.isEmpty()) {
                 // Standard license type (activation)
                 if (!pdfix.GetStandardAuthorization().Activate(licenseKey)) {
                     throw new Exception(pdfix.GetError());
                 }
-            }
-            else {
+            } else if (!licenseKey.isEmpty()) {
                 // Account license type
                 if (!pdfix.GetAccountAuthorization().Authorize(licenseName, licenseKey)) {
                     throw new Exception(pdfix.GetError());
-                }        
+                }
             }
         }
         // your code goes here
 
         pdfix.Destroy();
-    }  
+    }
 }
