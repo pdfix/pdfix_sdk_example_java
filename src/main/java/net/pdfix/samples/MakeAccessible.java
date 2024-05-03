d3f4648f34d779ef6cd4a52e54935e8b356b15f5
jbaranec <jozef.baranec@gmail.com>
2024-05-03 08:01:11 +0200

    MakeAccessible example update with default command

--- src/main/java/net/pdfix/samples/MakeAccessible.java
+++ src/main/java/net/pdfix/samples/MakeAccessible.java
@@ -10,35 +10,44 @@ package net.pdfix.samples;
 import net.pdfix.pdfixlib.*;
 
 public class MakeAccessible {
-    public static void run (
-      String openPath,
-      String savePath,
-      String commandPath
-    ) throws Exception {
+    public static void run(
+            String openPath,
+            String savePath,
+            String commandPath) throws Exception {
         Pdfix pdfix = new Pdfix();
         PdfDoc doc = pdfix.OpenDoc(openPath, "");
         if (doc == null)
             throw new Exception(pdfix.GetError());
 
-        PsCommand cmd = doc.GetCommand();
+        PsCommand command = doc.GetCommand();
 
-        // customize auto-tagging
-        PsFileStream stm = pdfix.CreateFileStream(commandPath, PsFileMode.kPsReadOnly);
-        if (stm == null) {
-            throw new Exception(pdfix.GetError());
-        }
-        if (!cmd.LoadParamsFromStream(stm, PsDataFormat.kDataFormatJson)) {
-            throw new Exception(pdfix.GetError());
-        }
-        stm.Destroy();
+        PsStream cmdStm = null;
 
-        if (!cmd.Run()) {
+        if (commandPath.isEmpty()) {
+            cmdStm = pdfix.CreateMemStream();
+            if ((cmdStm == null) || !command.SaveCommandsToStream(PsCommandType.kCommandMakeAccessible, cmdStm, PsDataFormat.kDataFormatJson,
+                                                        Pdfix.kSaveFull))
+            {
+                throw new Exception(pdfix.GetError());
+            }
+        } else {
+            PsFileStream stm = pdfix.CreateFileStream(commandPath, PsFileMode.kPsReadOnly);
+            if (stm == null) {
+                throw new Exception(pdfix.GetError());
+            }
+        }
+        if (!command.LoadParamsFromStream(cmdStm, PsDataFormat.kDataFormatJson)) {
             throw new Exception(pdfix.GetError());
         }
-        
+        cmdStm.Destroy();
+
+        if (!command.Run()) {
+            throw new Exception(pdfix.GetError());
+        }
+
         if (!doc.Save(savePath, Pdfix.kSaveFull))
             throw new Exception(pdfix.GetError());
-        
+
         doc.Close();
         pdfix.Destroy();
     }
