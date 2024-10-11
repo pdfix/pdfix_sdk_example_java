6802565c10c94d0b6634dd430163742754a106cb
jbaranec <jozef.baranec@gmail.com>
2024-10-11 23:33:35 +0200

    customized auto-tagging with 8.2

--- src/main/java/net/pdfix/App.java
+++ src/main/java/net/pdfix/App.java
@@ -11,6 +11,7 @@ public class App {
 
         static String testName = "";
 
+
         static boolean runTest(String name) {
                 boolean run = (testName.isEmpty() || (testName.compareTo(name) == 0));
                 if (run) {
@@ -30,154 +31,25 @@ public class App {
 
                 String inputPath = basePath + "/resources";
                 String outputPath = basePath + "/output";
-                String configPath = "";
 
                 if (!Files.exists(Paths.get(outputPath)))
                         Files.createDirectory(Paths.get(outputPath));
 
-                Initialization.run();
+                AddTags.run( inputPath + "/db/source1.pdf",
+                                inputPath + "/db/source1_tagged.pdf", 
+                                inputPath + "/db/template2.json");
 
-                if (runTest("GetBookmarks")) {
-                        GetBookmarks.run(inputPath + "/test.pdf");
-                }
+                AddTags.run( inputPath + "/db/source2.pdf",
+                                inputPath + "/db/source2_tagged.pdf", 
+                                inputPath + "/db/template2.json");
 
-                if (runTest("ParsePdsObjects")) {
-                        ParsePdsObjects.run(inputPath + "/test.pdf");
-                }
+                // MakeAccessible.run( inputPath + "/db/source1.pdf",
+                //                 inputPath + "/db/source1_command_tagged.pdf", 
+                //                 inputPath + "/db/command.json");
 
-                if (runTest("RenderPage")) {
-                        RenderPage.run(inputPath + "/test.pdf",
-                                        outputPath + "/RenderPage.jpg",
-                                        2.0f);
-                }
-                ////////////////////////////////////////////////////
-                // Basic, Professional, Enterprise version
-                if (runTest("AddComment")) {
-                        AddComment.run(inputPath + "/test.pdf",
-                                        outputPath + "/AddComment.pdf");
-                }
-
-                if (runTest("AddTags")) {
-                        System.out.println("AddTags");
-                        AddTags.run(inputPath + "/test.pdf",
-                                        outputPath + "/AddTags.pdf", configPath);
-                }
-
-                if (runTest("AddWatermark")) {
-                        AddWatermark.run(inputPath + "/test.pdf",
-                                        outputPath + "/AddWatermark.pdf",
-                                        inputPath + "/watermark.png");
-                }
-
-                if (runTest("ConvertToHtml")) {
-                        ConvertToHtml.run(inputPath + "/test.pdf",
-                                        outputPath + "/index.html", configPath,
-                                        new PdfHtmlParams());
-                }
-
-                if (runTest("ConvertToHtmlByPages")) {
-                        ConvertToHtmlByPages.run(inputPath + "/test.pdf");
-                }
-
-                if (runTest("DigitalSignature")) {
-                        DigitalSignature.run(inputPath + "/test.pdf",
-                                        outputPath + "/DigitalSignature.pdf",
-                                        inputPath + "/test.pfx", "TEST_PASSWORD");
-                }
-
-                if (runTest("DocumentMetadata")) {
-                        DocumentMetadata.run(inputPath + "/test.pdf",
-                                        outputPath + "/DocumentMetadata.pdf");
-                }
-
-                if (runTest("EmbedFonts")) {
-                        EmbedFonts.run(inputPath + "/test.pdf",
-                                        outputPath + "/EmbedFonts.pdf");
-                }
-
-                if (runTest("ExportFormFieldValues")) {
-                        ExportFormFieldValues.run(inputPath + "/test.pdf");
-                }
-
-                if (runTest("ExtractImages")) {
-                        ExtractImages.run(inputPath + "/test.pdf",
-                                        outputPath + "/",
-                                        800,
-                                        new PdfImageParams());
-                }
-
-                if (runTest("ExtractTables")) {
-                        ExtractTables.run(inputPath + "/test.pdf",
-                                        outputPath + "/",
-                                        configPath);
-                }
-
-                if (runTest("ExtractText")) {
-                        ExtractText.run(inputPath + "/test.pdf",
-                                        outputPath + "/ExtractText.txt",
-                                        configPath);
-                }
-
-                if (runTest("FlattenAnnots")) {
-                        FlattenAnnots.run(inputPath + "/test.pdf",
-                                        outputPath + "/FlattenAnnots.pdf");
-                }
-
-                if (runTest("GetWhitespace")) {
-                        GetWhitespace.run(inputPath + "/test.pdf");
-                }
-
-                if (runTest("MakeAccessible")) {
-                        MakeAccessible.run(inputPath + "/test.pdf",
-                                        outputPath + "/MakeAccessible.pdf",
-                                        basePath + "/resources/make-accessible.json");
-                }
-
-                // if (runTest("OcrWithTesseract")) {
-                //         OcrWithTesseract.run(inputPath + "/scanned.pdf",
-                //                         outputPath + "/OcrWithTesseract.pdf",
-                //                         inputPath + ("/tessdata"),
-                //                         "eng", 2.0F, PdfRotate.kRotate0);
-                // }
-
-                if (runTest("RegexSearch")) {
-                        RegexSearch.run(inputPath + "/test.pdf",
-                                        "(\\d{4}[- ]){3}\\d{4}");
-                }
-
-                if (runTest("ParsePdsObjects")) {
-                        RegexSetPattern.run(inputPath + "/test.pdf");
-                }
-
-                if (runTest("RemoveComments")) {
-                        RemoveComments.run(inputPath + "/test.pdf",
-                                        outputPath + "/RemoveComments.pdf");
-                }
-
-                if (runTest("SetFieldFlags")) {
-                        SetFieldFlags.run(inputPath + "/test.pdf",
-                                        outputPath + "/SetFieldFlags.pdf");
-                }
-
-                if (runTest("SetFormFieldValue")) {
-                        SetFormFieldValue.run(inputPath + "/test.pdf",
-                                        outputPath + "/SetFormFieldValue.pdf");
-                }
-
-                if (runTest("ThreadSafePdfix")) {
-                        ThreadSafePdfix.run(inputPath + "/test.pdf");
-                }
-
-                // tags samples
-                if (runTest("TagsEditStructTree")) {
-                        TagsEditStructTree.run(inputPath + "/test.pdf",
-                                        outputPath + "/TagsEditStructTree.pdf");
-                }
-
-                if (runTest("TagsReadStructureTree")) {
-                        TagsReadStructureTree.run(inputPath + "/test.pdf",
-                                        outputPath + "/TagsEditStructTree.pdf");
-                }
+                // MakeAccessible.run( inputPath + "/db/source2.pdf",
+                //                 inputPath + "/db/source2_command_tagged.pdf", 
+                //                 inputPath + "/db/command.json");
 
                 System.out.println("SUCCESS");
         }
--- src/main/java/net/pdfix/samples/AddTags.java
+++ src/main/java/net/pdfix/samples/AddTags.java
@@ -9,44 +9,65 @@ package net.pdfix.samples;
 import net.pdfix.pdfixlib.*;
 
 public class AddTags {
-    public static void run(
-      String openPath,                          // source PDF document
-      String savePath,                          // output HTML file
-      String configPath                         // configuration file
-    ) throws Exception {        
-        Pdfix pdfix = new Pdfix();
-        PdfDoc doc = pdfix.OpenDoc(openPath, "");
-        if (doc == null)
-            throw new Exception(pdfix.GetError());
+  public static void run(
+      String openPath, // source PDF document
+      String savePath, // output HTML file
+      String configPath // configuration file
+  ) throws Exception {
+    Pdfix pdfix = new Pdfix();
+    PdfDoc doc = pdfix.OpenDoc(openPath, "");
+    if (doc == null)
+      throw new Exception(pdfix.GetError());
 
-        // customize auto-tagging by loading a template configuration JSON
-        PsFileStream stm = pdfix.CreateFileStream(configPath, PsFileMode.kPsReadOnly);
-        if (stm != null) {
-            PdfDocTemplate doc_preflight = doc.GetTemplate();
-            if (doc_preflight == null)
-                throw new Exception(pdfix.GetError());
-            if (!doc_preflight.LoadFromStream(stm, PsDataFormat.kDataFormatJson))
-                throw new Exception(pdfix.GetError());
-            stm.Destroy();
-        }
-        else {
-            // customize auto-tagging by running preflight
-            PdfDocTemplate doc_preflight = doc.GetTemplate();
-            for (int i = 0; i < doc.GetNumPages(); i++) {
-                if (!doc_preflight.AddPage(i))
-                    throw new Exception(pdfix.GetError());
-            }
-            if (!doc_preflight.Update())
-                throw new Exception(pdfix.GetError());
-        }
+    // Clear document title. Document title will be set when taggging with value of
+    // H1 heading
+    if (!doc.SetInfo("Title", "")) {
+      throw new Exception(pdfix.GetError());
+    }
 
-        if (!doc.AddTags(new PdfTagsParams()))
-            throw new Exception(pdfix.GetError());
+    // customize auto-tagging by loading a template configuration JSON
+    PsFileStream stm = pdfix.CreateFileStream(configPath, PsFileMode.kPsReadOnly);
+    if (stm != null) {
+      PdfDocTemplate doc_preflight = doc.GetTemplate();
+      if (doc_preflight == null)
+        throw new Exception(pdfix.GetError());
+      if (!doc_preflight.LoadFromStream(stm, PsDataFormat.kDataFormatJson))
+        throw new Exception(pdfix.GetError());
+      stm.Destroy();
+    }
 
-        if (!doc.Save(savePath, Pdfix.kSaveFull | Pdfix.kSaveCompressedStructureOnly))
-            throw new Exception(pdfix.GetError());
+    // Autotag PDF with the loaded template
+    if (!doc.AddTags(new PdfTagsParams()))
+      throw new Exception(pdfix.GetError());
 
-        doc.Close();
-        pdfix.Destroy();
-    }  
+    // OPTIONAL: Merge tables spanned across multiple pages into sing Table 
+
+    // Set document language
+    if (!doc.SetLang("de"))
+      throw new Exception(pdfix.GetError());
+
+    // create bookmarks from headings
+    if (!doc.CreateBookmarks()) {
+      throw new Exception(pdfix.GetError());
+    }
+
+    // set PDF/UA comliance standard
+    if (!doc.SetPdfStandard(Pdfix.kPdfStandardPdfUA, "1")) {
+      throw new Exception(pdfix.GetError());
+    }
+
+    // set display fot titile preferences
+    PdsDictionary root = doc.GetRootObject();
+    PdsDictionary vp = root.GetDictionary("ViewerPreferences");
+    if (vp == null) {
+      vp = root.PutDict("ViewerPreferences");
+    }
+    vp.PutBool("DisplayDocTitle", true);
+
+    if (!doc.Save(savePath, Pdfix.kSaveFull | Pdfix.kSaveCompressedStructureOnly))
+      throw new Exception(pdfix.GetError());
+
+    doc.Close();
+    pdfix.Destroy();
+  }
 }
